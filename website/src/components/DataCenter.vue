e961c004ab3e8b9256f576d171ec8c18f0285808
ruyiqf <49644052@qq.com>
2017-12-21 09:17:52 +0800

    Adding nodejs for server

---
+++ website/src/components/DataCenter.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="datacenter">
+        <table>
+            <thead>
+            <tr>
+            <th>Regian</th>
+            <th>Q1 2010</th>
+            <th>Q2 2010</th>
+            <th>Q3 2010</th>
+            <th>Q4 2010</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tbody class="labels">
+                <tr>
+                    <td colspan="5">
+                        <label for="accounting">Accounting</label>
+                        <input type="checkbox" name="accounting" id="accounting" data-toggle="toggle">
+                    </td>
+                </tr>
+                </tbody>
+
+                <tbody class="hide">
+                    <tr>
+                        <td>Australia</td>
+                        <td>$7,685.00</td>
+                        <td>$3,544.00</td>
+                        <td>$5,834.00</td>
+                        <td>$10,583.00</td>
+                    </tr>
+                </tbody>
+            <tbody class="labels">
+                <tr>
+                    <td colspan="5">
+                        <label for="management">Management</label>
+                        <input type="checkbox" name="management" id="management" data-toggle="toggle">
+                    </td>
+                </tr>
+            </tbody>
+            <tbody class="hide">
+            <tr>
+                <td>Australia</td>
+                <td>$7,685.00</td>
+                <td>$3,544.00</td>
+                <td>$5,834.00</td>
+                <td>$10,583.00</td>
+            </tr>
+            </tbody>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'datacenter',
+      data () {
+        return {
+        }
+      }
+    }
+</script>
+<style>
+    table {
+        width: 750px;
+        border-collapse: collapse;
+        margin:50px auto;
+    }
+
+    th {
+        background: #3498db;
+        color: white;
+        font-weight: bold;
+    }
+
+    td, th {
+        padding: 10px;
+        border: 1px solid #ccc;
+        text-align: left;
+        font-size: 18px;
+    }
+
+    .labels tr td {
+        background-color: #2cc16a;
+        font-weight: bold;
+        color: #fff;
+    }
+
+    .label tr td label {
+        display: block;
+    }
+
+
+    [data-toggle="toggle"] {
+        display: none;
+    }
+</style>
--- website/src/server/serverinf_express.js
+++ website/src/server/serverinf_express.js
@@ -12,6 +12,5 @@ app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
-
     console.log(`server is listening on ${port}`)
 })
